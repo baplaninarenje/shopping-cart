60337c9c4a6bed116eca2ba930ae8536fb9b09ac
baplaninarenje <157408243+baplaninarenje@users.noreply.github.com>
2025-01-14 16:29:16 +0100

    Update Home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,27 @@
+import { Link } from 'react-router-dom';
+
 const Home = () => {
-  return <>Home</>;
+  return (
+    <>
+      <h2>Explore Popular Categories</h2>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/products/electronics">electronics</Link>
+          </li>
+          <li>
+            <Link to="/products/jewelery">jewelery</Link>
+          </li>
+          <li>
+            <Link to="/products/mens-clothing">men&apos;s clothing</Link>
+          </li>
+          <li>
+            <Link to="/products/womens-clothing">women&apos;s clothing</Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 };
 
 export default Home;
