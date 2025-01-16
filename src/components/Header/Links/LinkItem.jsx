25d784da68633e9fc03d9eac7847122c474fde38
baplaninarenje <157408243+baplaninarenje@users.noreply.github.com>
2025-01-16 15:31:19 +0100

    Lift up products fetching and handle browser navigation actions

--- src/components/Header/Links/LinkItem.jsx
+++ src/components/Header/Links/LinkItem.jsx
@@ -1,38 +1,9 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { useContext } from 'react';
-import { getRequestWithNativeFetch } from '../../getRequestWithNativeFetch';
-import { ProductsContext } from '../../../contexts/ProductsContext';
-
-const baseUrl = 'https://fakestoreapi.com';
 
 const LinkItem = ({ to, children }) => {
-  const { setProducts, setLoading, setErrorMessage } =
-    useContext(ProductsContext);
-
-  const fetchDataForProducts = async (dynamicPathParam) => {
-    try {
-      const productsData = await getRequestWithNativeFetch(
-        baseUrl + dynamicPathParam
-      );
-      setProducts(Array.isArray(productsData) ? productsData : [productsData]);
-      setErrorMessage('');
-    } catch (err) {
-      setErrorMessage(err.message);
-      setProducts([]);
-      console.error(err);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  function handleClick() {
-    if (!to.includes('products')) return;
-    fetchDataForProducts(to);
-  }
-
   return (
-    <li onClick={handleClick}>
+    <li>
       <Link to={to}>{children}</Link>
     </li>
   );
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,12 +2,40 @@ import PropTypes from 'prop-types';
 import Header from './../Header/Header.jsx';
 import Footer from './../Footer/Footer.jsx';
 import { ProductsContext } from '../../contexts/ProductsContext.js';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { getRequestWithNativeFetch } from '../getRequestWithNativeFetch.js';
+
+const baseUrl = 'https://fakestoreapi.com';
 
 function Layout({ children }) {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [errorMessage, setErrorMessage] = useState('');
+
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    if (!pathname.includes('products')) return;
+    const fetchDataForProducts = async (dynamicPathParam) => {
+      try {
+        const productsData = await getRequestWithNativeFetch(
+          baseUrl + dynamicPathParam
+        );
+        setProducts(
+          Array.isArray(productsData) ? productsData : [productsData]
+        );
+        setErrorMessage('');
+      } catch (err) {
+        setErrorMessage(err.message);
+        setProducts([]);
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchDataForProducts(pathname);
+  }, [pathname]);
 
   return (
     <>
