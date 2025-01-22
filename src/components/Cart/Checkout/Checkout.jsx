567942754e248e94ef7283fa7294ca10d571e880
baplaninarenje <157408243+baplaninarenje@users.noreply.github.com>
2025-01-22 19:05:10 +0100

    Style Checkout

--- src/components/Cart/Checkout/Checkout.jsx
+++ src/components/Cart/Checkout/Checkout.jsx
@@ -1,6 +1,8 @@
+import styles from './Checkout.module.css';
+
 const Checkout = () => {
   return (
-    <div className="checkout">
+    <div className={styles.checkout}>
       <h3>Checkout</h3>
       <p>Work in progress, not implemented yet.</p>
     </div>
