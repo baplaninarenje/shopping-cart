c05538d64354e8a6f6a3086e771dbeb3943f8387
baplaninarenje <157408243+baplaninarenje@users.noreply.github.com>
2025-01-26 19:11:54 +0100

    All device styles for the ErrorPage and NothingFound

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,18 +1,26 @@
 import { Link, useRouteError } from 'react-router-dom';
+import styles from './ErrorPage.module.css';
 
 const ErrorPage = () => {
   const error = useRouteError();
 
   return (
-    <section className="error-page">
-      <h2>
-        {error?.message ||
-          error?.data ||
-          'Something went wrong. Please try again later.'}
-      </h2>
-      <Link to="/">
-        You can go back to the home page by clicking here, though!
-      </Link>
+    <section className={styles.errorPage}>
+      <div className={styles.errorText}>
+        <h2>
+          {error?.message ||
+            error?.data ||
+            'Something went wrong. Please try again later.'}
+        </h2>
+        <Link to="/">Go to the homepage</Link>
+      </div>
+      <img
+        className={styles.errorImg}
+        width={203}
+        height={243}
+        src="http://i.ebayimg.com/00/s/MTUwMFgxMjAw/z/8ScAAOSw-ANlzRIW/$_1.PNG?set_id=2"
+        alt="Error"
+      />
     </section>
   );
 };
--- src/components/Products/NothingFound/NothingFound.jsx
+++ src/components/Products/NothingFound/NothingFound.jsx
@@ -1,13 +1,23 @@
 import { Link } from 'react-router-dom';
+import styles from './../../ErrorPage/ErrorPage.module.css';
 
 const NothingFound = () => {
   return (
-    <section className="nothing-found">
-      <h2>We looked everywhere.</h2>
-      <p>It looks like this page is missing, or there are no search results.</p>
-      <Link to="/">
-        You can go back to the home page by clicking here, though!
-      </Link>
+    <section className={styles.errorPage}>
+      <div className={styles.errorText}>
+        <h2>We looked everywhere.</h2>
+        <p>
+          It looks like this page is missing, or there are no search results.
+        </p>
+        <Link to="/">Go to the homepage</Link>
+      </div>
+      <img
+        className={styles.errorImg}
+        width={203}
+        height={243}
+        src="http://i.ebayimg.com/00/s/MTUwMFgxMjAw/z/8ScAAOSw-ANlzRIW/$_1.PNG?set_id=2"
+        alt="Error"
+      />
     </section>
   );
 };
