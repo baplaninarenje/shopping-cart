b4bef53afdfb1e6a2f0035f1c877a9df92760ddb
baplaninarenje <157408243+baplaninarenje@users.noreply.github.com>
2025-01-30 19:42:35 +0100

    Update cart link to be a cart icon instead of the text

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -26,10 +26,25 @@ function Header() {
     {
       to: '/cart',
       children: (
-        <>
-          <span>Cart</span>
+        <div className={styles.cartContainer}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="1.5em"
+            height="1.5em"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="rgb(17, 24, 32)"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="lucide lucide-shopping-cart"
+          >
+            <circle cx="9" cy="21" r="1" />
+            <circle cx="20" cy="21" r="1" />
+            <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6" />
+          </svg>
           <span className={styles.cartIcon}>{cartItemsCount}</span>
-        </>
+        </div>
       ),
     },
   ];
